afdb4b27f214e0f762a479d7c3ff09e0d4d48af7
Stephan Borg <wolff_borg@users.sourceforge.net>
2005-08-12 11:38:09 +0000

    Added BITCONTACT content type

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -2,6 +2,8 @@
 global $gBitSystem, $smarty;
 $gBitSystem->registerPackage( 'contacts', dirname( __FILE__).'/' );
 
+define('BITCONTACT_CONTENT_TYPE_GUID', 'bitcontact' );
+
 if( $gBitSystem->isPackageActive( 'contacts' ) ) {
 	$gBitSystem->registerAppMenu( 'contacts', 'Contact', CONTACTS_PKG_URL.'index.php', 'bitpackage:contacts/menu_contact.tpl', 'contacts');
 }
